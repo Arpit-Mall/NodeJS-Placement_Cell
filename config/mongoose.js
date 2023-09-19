5984e66d8b7d50a2dfc3b6bb4670f38d11af96ed
Arpit-Mall <arpitmall02@gmail.com>
2023-09-19 23:19:18 +0530

    Project Completed

---
+++ config/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+//connecting mongoose with database
+const connectToMongo = async () => {
+	const connectdb = await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+		.then(() => { console.log("Connected to MongoDB Successfully") })
+		.catch((error) => { console.log('Database not connected successfully') })
+}
+
+module.exports = connectToMongo;
